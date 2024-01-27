2aa0a83c7e34fefe61d982d741242ac4269190e3
Afnan312 <tanhiatfatema@gmail.com>
2024-01-28 01:15:58 +0600

    [FEAT]: added voucher management from admin side

--- backend/routes/api/admin.js
+++ backend/routes/api/admin.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const adminUserControl = require('../../controllers/adminUserController');
+const adminVoucherControl = require('../../controllers/adminVoucherController');
 
+//manage users
 router.get('/users/:userId/delete', adminUserControl.deleteUser);
 router.get('/users/:userId', adminUserControl.getOneUser);
 router.get('/users', adminUserControl.getUsers);
+
+//manage vouchers
+router.post('/vouchers/add', adminVoucherControl.addVoucher);
+router.get('/vouchers/:voucherId', adminVoucherControl.viewOneVoucher);
+router.get('/vouchers', adminVoucherControl.viewAllVouchers);
+router.get('/vouchers/:voucherId/delete', adminVoucherControl.deleteVoucher);
 
 module.exports = router;
