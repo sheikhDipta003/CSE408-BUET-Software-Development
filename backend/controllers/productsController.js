470a336e1a374a04359d6796e3f03baccc2b93ec
Afnan312 <tanhiatfatema@gmail.com>
2024-01-28 10:43:20 +0600

    [FIX]: user profile and query search

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,39 +1,8 @@
 const Product = require('../models/Product');
 const ProductWebsite = require('../models/ProductWebsite');
 
-// Create a new product
-exports.createProduct = async (req, res) => {
-    try {
-        // Extract data from request body
-        const { productName, brand, imagePath, category, subcategory, color, width, height, images, websites } = req.body;
-
-        // Create the product
-        const product = await Product.create({
-            productName,
-            brand,
-            imagePath,
-            category,
-            subcategory,
-            color,
-            width,
-            height
-        });
-
-        // Create product images
-        await ProductImage.bulkCreate(images.map(image => ({ imageUrl: image.imageUrl, productId: product.productId })));
-
-        // Create product websites
-        await ProductWebsite.bulkCreate(websites.map(website => ({ ...website, productId: product.productId })));
-
-        res.status(201).json({ message: 'Product created successfully', product });
-    } catch (error) {
-        console.error('Error creating product:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-};
-
 // Get products by category and subcategory
-exports.getProductsByCategoryAndSubcategory = async (req, res) => {
+const getProductsByCategoryAndSubcategory = async (req, res) => {
     try {
         const { category, subcategory } = req.query;
 
@@ -41,11 +10,7 @@ exports.getProductsByCategoryAndSubcategory = async (req, res) => {
         console.log(subcategory);
 
         const products = await Product.findAll({
-            where: { category, subcategory },
-            include: [
-                { model: ProductImage },
-                { model: ProductWebsite }
-            ]
+            where: { category:category, subcategory:subcategory }
         });
 
         res.status(200).json({ products });
@@ -55,20 +20,4 @@ exports.getProductsByCategoryAndSubcategory = async (req, res) => {
     }
 };
 
-// Delete a product by productId
-exports.deleteProduct = async (req, res) => {
-    try {
-        const { productId } = req.params;
-
-        // Delete product and associated images and websites (cascading delete)
-        await Product.destroy({
-            where: { productId },
-            cascade: true
-        });
-
-        res.status(200).json({ message: 'Product deleted successfully' });
-    } catch (error) {
-        console.error('Error deleting product:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-};
+module.exports={getProductsByCategoryAndSubcategory};
