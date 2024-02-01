1f249ab27e9ea800fb04c6d3b51338585ce1ccc6
Afnan312 <tanhiatfatema@gmail.com>
2024-02-01 19:03:44 +0600

    [FEAT]: added product search and ordering

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,17 +1,126 @@
 const Product = require("../models/Product");
 const Website = require("../models/Website");
 const ProductWebsite = require("../models/ProductWebsite");
+const ProductPrice = require("../models/ProductPrice");
+const { Op, Sequelize } = require("sequelize");
 
 // Get products by category and subcategory
+const getProductByQuery = async (req, res) => {
+  try{
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const keyword = req.query.keyword || '';
+    const orderQ = req.query.order || 'ASC';
+    const order = orderQ.toUpperCase();
+    const offset = (page-1)*limit;
+
+    console.log(keyword);
+
+    const products = await Product.findAll({
+      where: {
+        name: {
+          //[Op.substring] : keyword,
+          [Op.match]: Sequelize.fn('plainto_tsquery', keyword),
+        }
+      },
+      include: [
+        {
+          model: ProductWebsite,
+          //attributes :[],
+          include: [
+            {
+              model: ProductPrice,
+              attributes: [[Sequelize.fn('MIN', Sequelize.col('price')), 'minPrice']],
+            }
+          ]
+        }
+      ],
+      group: ['Product.productId'],
+      order: [[Sequelize.literal('"ProductWebsite.ProductPrice.minPrice"'), order]],
+      offset: offset,
+      limit: limit,
+    });
+    res.status(200).json({ products });
+  }catch(error){
+    console.error("Error getting products:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 const getProductsByCategoryAndSubcategory = async (req, res) => {
   try {
     const { category, subcategory } = req.params;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const orderQ = req.query.order || 'ASC';
+    const order = orderQ.toUpperCase();
+    const offset = (page-1)*limit;
 
     console.log(category);
     console.log(subcategory);
 
     const products = await Product.findAll({
-      where: { category: category, subcategory: subcategory },
+      where: { 
+        category: category, 
+        subcategory: subcategory 
+      },
+      include: [
+        {
+          model: ProductWebsite,
+          //attributes :[],
+          include: [
+            {
+              model: ProductPrice,
+              attributes: [[Sequelize.fn('MIN', Sequelize.col('price')), 'minPrice']],
+            }
+          ]
+        }
+      ],
+      group: ['Product.productId'],
+      order: [[Sequelize.literal('"ProductWebsite.ProductPrice.minPrice"'), order]],
+      offset: offset,
+      limit: limit,
+    });
+
+    res.status(200).json({ products });
+  } catch (error) {
+    console.error("Error getting products:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+const getProductsByCategory = async (req, res) => {
+  try {
+    const { category } = req.params;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const orderQ = req.query.order || 'ASC';
+    const order = orderQ.toUpperCase();
+    const offset = (page-1)*limit;
+
+    console.log(category);
+
+    const products = await Product.findAll({
+      where: { 
+        category: category, 
+      },
+      include: [
+        {
+          model: ProductWebsite,
+          //attributes :[],
+          include: [
+            {
+              model: ProductPrice,
+              attributes: [[Sequelize.fn('MIN', Sequelize.col('price')), 'minPrice']],
+            }
+          ]
+        }
+      ],
+      group: ['Product.productId'],
+      order: [[Sequelize.literal('"ProductWebsite.ProductPrice.minPrice"'), order]],
+      offset: offset,
+      limit: limit,
     });
 
     res.status(200).json({ products });
@@ -91,6 +200,8 @@ const getProductWebsite = async (req, res) => {
 };
 
 module.exports = {
+  getProductByQuery,
+  getProductsByCategory,
   getProductsByCategoryAndSubcategory,
   getProductDetails,
   getProductWebsite,
